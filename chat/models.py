1967930703d8a710995f7f29b4330bc528106aac
DeepankumarTpDev <deepankumar@testpress.in>
2024-10-14 13:12:53 +0530

    Make model changes in chats for better Message Functionality creation (#6)

--- chat/models.py
+++ chat/models.py
@@ -3,23 +3,22 @@ from django.db import models
 from django.utils import timezone
 from ads.models import Ads
 
+
 class Chat(models.Model):
+    ad = models.ForeignKey(Ads, on_delete=models.CASCADE)  
+    created_on = models.DateTimeField(auto_now_add=True)  
+    users = models.ManyToManyField(User) 
+
+    def __str__(self):
+        return f"Chat for {self.ad} with users {', '.join([user.username for user in self.users.all()])}"
+
+
+class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', blank=False, null=False, on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', blank=False, null=False, on_delete=models.CASCADE)
     message = models.TextField()
-    ad = models.ForeignKey(Ads, on_delete=models.CASCADE, related_name='chats', null=False, blank=False)
-    timestamp = models.DateTimeField(default=timezone.now)
-    conversation_id = models.CharField(max_length=255, editable=False, null= False)
-
-    def save(self, *args, **kwargs):
-        if not self.conversation_id:
-            self.conversation_id = f'{self.sender.username}_{self.sender.id}_to_{self.receiver.username}_{self.receiver.id}'
-        
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.conversation_id
-
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages', null=False, blank=False)
+    created_on  = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['timestamp']
+        ordering = ['created_on']
